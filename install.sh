7a51b8e2c391e58f5b8dbf45badff9c5818b579e
Petr Stepanov <stepanovps@gmail.com>
2021-02-10 02:01:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,4 +48,4 @@ gsettings set org.gnome.terminal.legacy.keybindings close-window "['<Primary>q']
 gsettings set org.gnome.terminal.legacy.keybindings find "['<Primary>f']"
 
 # Disable Left Super Overlay Shortcut
-gsettings set org.gnome.mutter overlay-key "['Super_R']"
+gsettings set org.gnome.mutter overlay-key 'Super_R'

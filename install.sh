08efd8e981205e6e6c1ef0aa4cff736641139b2d
Petr Stepanov <stepanovps@gmail.com>
2021-02-22 15:18:52 -0500

    Lol

--- install.sh
+++ install.sh
@@ -14,38 +14,3 @@ fi
 # Install Autokey repository
 mkdir -p ~/.config/autokey/data
 cp -r ./gnome-macos-phrases ~/.config/autokey/data/
-
-# Tweak standard GNOME keybindings
-gsettings set org.gnome.desktop.wm.keybindings activate-window-menu "[]"
-gsettings set org.gnome.desktop.wm.keybindings close "['<Primary>w']"
-gsettings set org.gnome.desktop.wm.keybindings panel-main-menu "['LaunchA']"
-gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Primary>d']"
-gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Primary>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Primary><Shift>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-group "['<Primary>grave']"
-gsettings set org.gnome.desktop.wm.keybindings switch-group-backward "['<Primary><Shift>grave']"
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source "[]"
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "[]"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Right']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Left']"
-
-gsettings set org.gnome.shell.keybindings toggle-application-view "['LaunchB']"
-gsettings set org.gnome.shell.keybindings toggle-message-tray "[]"
-
-gsettings set org.gnome.mutter.keybindings toggle-tiled-left "[]"
-gsettings set org.gnome.mutter.keybindings toggle-tiled-right "[]"
-
-gsettings set org.gnome.mutter.wayland.keybindings restore-shortcuts "[]"
-
-gsettings set org.gnome.settings-daemon.plugins.media-keys window-screenshot "['<Primary><Shift>numbersign']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys area-screenshot "['<Primary><Shift>dollar']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "[]"
-
-gsettings set org.gnome.terminal.legacy.keybindings new-tab "['<Primary>t']"
-gsettings set org.gnome.terminal.legacy.keybindings new-window "['<Primary>n']"
-gsettings set org.gnome.terminal.legacy.keybindings close-tab "['<Primary>w']"
-gsettings set org.gnome.terminal.legacy.keybindings close-window "['<Primary>q']"
-gsettings set org.gnome.terminal.legacy.keybindings find "['<Primary>f']"
-
-# Disable Left Super Overlay Shortcut
-gsettings set org.gnome.mutter overlay-key 'Super_R'
